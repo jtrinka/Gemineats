edd6b5eca2ee6fdfd8b250a4f4a1723496dcef18
Jordan Trinka <jordantrinka4@hotmail.com>
2024-01-17 23:34:25 -0500

    Read in API key with streamlit for streamlit app

--- Gemineats.py
+++ Gemineats.py
@@ -1,12 +1,13 @@
 import json
 from lib.streamlit.streamlit import cs_body, cs_sidebar
+import streamlit as st
 
 if __name__ == "__main__":
     with open(file="./configs/default.json") as f:
        config = json.load(f)
 
     prompt_data_config = config["prompt_data_config"]
-    GOOGLE_API_KEY = config["GOOGLE_API_KEY"]
+    GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
     prompt_data_config, generate_recipe_bool=cs_sidebar()
     cs_body(GOOGLE_API_KEY=GOOGLE_API_KEY, 
             prompt_data_config=prompt_data_config,
